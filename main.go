d8000c812d19af144d50594ab4bc7b565927f1e7
Anddd7 <anddd7@icloud.com>
2024-05-21 13:32:44 +0800

    ✨ feat: simulate the chain

--- main.go
+++ main.go
@@ -3,42 +3,81 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
 	fmt.Println("Hello, blockchain")
 
+	// first, you need to create a new chain or
+	// load an existing chain from the disk or network
+	chain := NewChain(1)
+
+	printJson("loaded chain", chain)
+
+	// then, you start a node with the chain
 	node := Node{
-		Chain:      Chain{},
-		LatestHash: Hash{GENESIS_NONCE},
+		Chain: chain,
 	}
 
-	printJson("created node", node)
+	printJson("started node", node)
 
-	transactions := []Transaction{
-		{
-			From:  Wallet{Address: "me", Balance: Money{Amount: 100}},
-			To:    Wallet{Address: "you", Balance: Money{Amount: 0}},
-			Value: Money{Amount: 100},
-		},
-	}
-	block := node.NewBlock(transactions, Metadata{
-		"timestamp": time.Now().String(),
-	})
+	// now, you can execute transactions in a new block
+	blocks := make(chan Block)
+	stop := make(chan struct{})
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(1)
 
-	printJson("created block", block)
+	// each block contains a list of transactions and metadata
+	go func() {
+		blocks <- node.NewBlock("Block 1", []Transaction{}, Metadata{})
+		blocks <- node.NewBlock("Block 2", []Transaction{}, Metadata{})
+		blocks <- node.NewBlock("Block 3", []Transaction{
+			{
+				From:  Wallet{Address: "me", Balance: Money{Amount: 100}},
+				To:    Wallet{Address: "you", Balance: Money{Amount: 0}},
+				Value: Money{Amount: 100},
+			},
+		}, Metadata{})
+		stop <- struct{}{}
+	}()
 
-	block, _ = node.Nonce(block)
+	// when a block is created, the node will try to "mine" it -
+	// find a nonce that satisfies the difficulty level
+	// then broadcast the block to the network to be added to the chain globally
+	go func() {
+		defer waitGroup.Done()
+		for {
+			select {
+			case block := <-blocks:
+				printJson("created block", block)
 
-	printJson("mined block", block)
+				signed, _ := node.Nonce(block)
 
-	_ = node.Broadcast(block)
+				printJson("signed block", signed)
 
-	printJson("broadcasted block", block)
+				_ = node.Broadcast(signed)
+				node.Chain.AddBlock(signed)
+
+				fmt.Printf("block %s added to the chain\n", signed.Metadata["name"])
+			case <-stop:
+				return
+			}
+		}
+	}()
+
+	waitGroup.Wait()
+
+	printTable(node.Chain)
 }
 
 func printJson(msg string, obj interface{}) {
 	json, _ := json.Marshal(obj)
 	fmt.Printf("%s: %v\n", msg, string(json))
+}
+
+func printTable(chain Chain) {
+	for _, block := range chain.Blocks {
+		fmt.Printf("%s\t%s\t%s\n", block.Metadata["name"], block.Hash, block.Timestamp)
+	}
 }
--- model.go
+++ model.go
@@ -5,62 +5,20 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"time"
 )
 
 const (
 	GENESIS_NONCE = "000000"
 )
 
-// Node the agent hosting the blockchain networking
-type Node struct {
-	Chain      Chain
-	LatestHash Hash
-}
-
-// NewBlock create a new block
-func (n *Node) NewBlock(transactions []Transaction, metadata Metadata) Block {
-	block := Block{
-		PreHash:      n.LatestHash,
-		Transactions: transactions,
-		Metadata:     metadata,
-	}
-	return block
-}
-
-// Nonce the proof of work
-func (n *Node) Nonce(block Block) (Block, error) {
-	value, err := block.toString()
-	if err != nil {
-		return Block{}, err
-	}
-	nouce := hash(value)
-	block.Hash.Nouce = nouce
-
-	return block, nil
-}
-
-func hash(value string) string {
-	hasher := sha256.New()
-	hasher.Write([]byte(value))
-	return hex.EncodeToString(hasher.Sum(nil))
-}
-
-// Broadcast the block to the network
-func (n *Node) Broadcast(block Block) error {
-	return errors.New("Not implemented")
-}
-
-// Block the data structure representing each block in the blockchain
+// Block the data structure that represents the block in the blockchain
 type Block struct {
 	Hash         Hash
 	PreHash      Hash
 	Transactions []Transaction
 	Metadata     Metadata
-}
-
-func (b *Block) toString() (string, error) {
-	bytes, err := json.Marshal(b)
-	return string(bytes), err
+	Timestamp    time.Time
 }
 
 // Hash the indentifier of the block
@@ -89,7 +47,85 @@ type Money struct {
 	Amount int
 }
 
+// toString capture and summarize the block data
+func (b *Block) toString() (string, error) {
+	bytes, err := json.Marshal(b)
+	return string(bytes), err
+}
+
 // Chain the chain of blocks
 type Chain struct {
-	Blocks map[Hash]Block
+	Difficulty int
+	Blocks     []Block
+}
+
+// NewChain create a new chain with genesis block
+func NewChain(difficulty int) Chain {
+	return Chain{
+		Difficulty: difficulty,
+		Blocks: []Block{
+			{
+				Hash:     Hash{Nouce: GENESIS_NONCE},
+				Metadata: Metadata{"name": "Genesis Block"},
+				Timestamp: time.Now(),
+			},
+		},
+	}
+}
+
+// AddBlock add a new block to the chain after broadcast is successful
+func (c *Chain) AddBlock(block Block) {
+	c.Blocks = append(c.Blocks, block)
+}
+
+// LatestHash get the latest hash of the chain
+func (c *Chain) LatestHash() Hash {
+	return c.Blocks[len(c.Blocks)-1].Hash
+}
+
+// Node the agent that hosts blockchain network
+type Node struct {
+	Chain Chain
+}
+
+// NewBlock create a new block in current node
+func (n *Node) NewBlock(name string, transactions []Transaction, metadata Metadata) Block {
+	metadata["name"] = name
+
+	return Block{
+		PreHash:      n.Chain.LatestHash(),
+		Transactions: transactions,
+		Metadata:     metadata,
+		Timestamp:    time.Now(),
+	}
+}
+
+// Nonce "mine" the block by finding the nonce
+func (n *Node) Nonce(block Block) (Block, error) {
+	value, err := block.toString()
+	if err != nil {
+		return Block{}, err
+	}
+	nouce := n.hash(value)
+	block.Hash.Nouce = nouce
+
+	return block, nil
+}
+
+// hash the proof of work function
+func (n *Node) hash(value string) string {
+	// use sleep to simulate the proof of work
+	// high difficulty means longer time to compute
+	// when multiple nodes are competing to mine the block
+	// only the node with the highest computation power will win
+	time.Sleep(time.Second * time.Duration(n.Chain.Difficulty))
+
+	hasher := sha256.New()
+	hasher.Write([]byte(value))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+// Broadcast the block to the network
+func (n *Node) Broadcast(block Block) error {
+	return errors.New("Not implemented")
 }
