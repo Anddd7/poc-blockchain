94ae5c53e249a51f59d4e622fa53369a86e6b2c5
Anddd7 <anddd7@icloud.com>
2024-05-21 10:59:32 +0800

    ✨ feat: create block with nonce

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello, blockchain")
+
+	node := Node{
+		Chain:      Chain{},
+		LatestHash: Hash{GENESIS_NONCE},
+	}
+
+	printJson("created node", node)
+
+	transactions := []Transaction{
+		{
+			From:  Wallet{Address: "me", Balance: Money{Amount: 100}},
+			To:    Wallet{Address: "you", Balance: Money{Amount: 0}},
+			Value: Money{Amount: 100},
+		},
+	}
+	block := node.NewBlock(transactions, Metadata{
+		"timestamp": time.Now().String(),
+	})
+
+	printJson("created block", block)
+
+	block, _ = node.Nonce(block)
+
+	printJson("mined block", block)
+
+	_ = node.Broadcast(block)
+
+	printJson("broadcasted block", block)
+}
+
+func printJson(msg string, obj interface{}) {
+	json, _ := json.Marshal(obj)
+	fmt.Printf("%s: %v\n", msg, string(json))
 }
--- model.go
+++ model.go
@@ -1,13 +1,48 @@
 package main
 
-import "errors"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+)
+
+const (
+	GENESIS_NONCE = "000000"
+)
 
 // Node the agent hosting the blockchain networking
-type Node struct{}
+type Node struct {
+	Chain      Chain
+	LatestHash Hash
+}
+
+// NewBlock create a new block
+func (n *Node) NewBlock(transactions []Transaction, metadata Metadata) Block {
+	block := Block{
+		PreHash:      n.LatestHash,
+		Transactions: transactions,
+		Metadata:     metadata,
+	}
+	return block
+}
 
 // Nonce the proof of work
-func (n *Node) Nonce(block Block) error {
-	return errors.New("Not implemented")
+func (n *Node) Nonce(block Block) (Block, error) {
+	value, err := block.toString()
+	if err != nil {
+		return Block{}, err
+	}
+	nouce := hash(value)
+	block.Hash.Nouce = nouce
+
+	return block, nil
+}
+
+func hash(value string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(value))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 // Broadcast the block to the network
@@ -23,6 +58,11 @@ type Block struct {
 	Metadata     Metadata
 }
 
+func (b *Block) toString() (string, error) {
+	bytes, err := json.Marshal(b)
+	return string(bytes), err
+}
+
 // Hash the indentifier of the block
 type Hash struct {
 	Nouce string
@@ -32,7 +72,24 @@ type Hash struct {
 type Metadata map[string]string
 
 // Transaction the transaction data structure
-type Transaction struct{}
+type Transaction struct {
+	From  Wallet
+	To    Wallet
+	Value Money
+}
+
+// Wallet the wallet data structure
+type Wallet struct {
+	Address string
+	Balance Money
+}
+
+// Money the money data structure
+type Money struct {
+	Amount int
+}
 
 // Chain the chain of blocks
-type Chain struct{}
+type Chain struct {
+	Blocks map[Hash]Block
+}
